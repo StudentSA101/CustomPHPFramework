94257582fa1735637f28af6c3af5a553149f4d35
Pierre <cpierredavis@gmail.com>
2018-12-12 21:16:44 +0200

    works locally

--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,5 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+require './../../vendor/autoload.php';
 
-require_once __DIR__.'/../app/core/bootstrap.php';
+require_once './../core/bootstrap.php';

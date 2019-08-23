aefa3a50eae436da5c2a6b0f42c2d2ee7a941c5a
pierre <cpierredavis@gmail.com>
2019-08-24 01:43:43 +0200

    Z-4 | Set out functionally how the interactive console will work, started to split the automated command from the interactive command

---
+++ app/commands/automatedCommand.php
@@ -0,0 +1,3 @@
+<?php
+
+echo 'automated command has been opened';
---
+++ app/commands/interactiveCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Welcome, how many parking lots do you have?";
+
+$parkingLot = fread(STDIN, 80);
+
+$parkingLot = trim($site);
+
+if (count($parkingLot) > 0) {
+
+    echo "Are you sure that one is your favorite?n";
+
+} else {
+
+    throw new Exception('Sorry, please try again and enter an amount bigger than 1');
+
+}

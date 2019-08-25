dade7ee4eddf9c9446ad5b891c847bc6d05879fe
pierre <cpierredavis@gmail.com>
2019-08-25 11:26:13 +0200

    Z-21 | added Insert functionality for parking

--- app/Models/VehicleModal.php
+++ app/Models/VehicleModal.php
@@ -9,20 +9,40 @@ class VehicleModal
     public static function create($registration, $colour)
     {
         try {
-            $query = "SELECT 'id' FROM slots asc LIMIT 3";
+            $query = "SELECT registration_number AS Registration_Number FROM vehicles WHERE registration_number = :existing";
             $statement = Container::get('database')->prepare($query);
-
+            $statement->bindParam(':id', $firstOpenSlot);
             $statement->execute();
-            $lotDetails = $statement->fetch();
-
+            $existing = $statement->fetchAll();
         } catch (PDOException $e) {
             print "Error!: " . $e->getMessage() . "<br/>";
             die();
         }
-        $query = "INSERT INTO vehicles(slots_id,registration_number,colour) VALUES($openParking,$registration,$colour)";
-        $statement = Container::get('database')->prepare($query)->$statement->execute();
+        if (count($existing) > 0) {
+            try {
+                $query = "SELECT id AS Parking_Slot FROM slots WHERE active = 'TRUE' LIMIT 1 ";
+                $statement = Container::get('database')->prepare($query);
+                $statement->execute();
+                $firstOpenSlot = $statement->fetchAll()[0]['Parking_Slot'];
+            } catch (PDOException $e) {
+                print "Error!: " . $e->getMessage() . "<br/>";
+                die();
+            }
+            try {
+                $query = "INSERT INTO vehicles(slots_id,registration_number,vehicle_colour) VALUES(:id,:registration,:colour)";
+                $statement = Container::get('database')->prepare($query);
+                $statement->bindParam(':id', $firstOpenSlot);
+                $statement->bindParam(':registration', $registration);
+                $statement->bindParam(':colour', $colour);
+                $statement->execute();
+            } catch (PDOException $e) {
+                print "Error!: " . $e->getMessage() . "<br/>";
+                die();
+            }
 
-        return $openParking;
+        }
+
+        return $firstOpenSlot;
     }
 
 }

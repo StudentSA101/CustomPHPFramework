29fb9af8d6241b9b4d1bb9650668f4cc6e9dc059
pierre <cpierredavis@gmail.com>
2020-01-19 21:16:27 +0200

    latest commit

--- app/Repository/GetFileContent.php
+++ app/Repository/GetFileContent.php
@@ -17,7 +17,7 @@ class GetFileContent implements GetFileContentsInterface
     public function get(): array
     {
         return preg_split('/\n/', file_get_contents(
-            "/" . trim(__DIR__, 'app/Repository/app/Commands') . "/storage/" . $_SERVER["argv"][1]));
+            "/" . dirname(dirname(dirname(__FILE__))) . "/storage/" . $_SERVER["argv"][1]));
     }
 
 }
